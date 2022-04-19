772f2b1a40fa64e0e10c1b38c96f3973cec4e1cf
joeyudongs <yudongs13@gmail.com>
2022-04-18 20:33:23 -0500

    frontend init

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <hi>{{msg}}</hi>
+    <el-button type="danger">{{ msg }}</el-button>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: "hello SE452"
+    }
+  }
+}
+</script>
