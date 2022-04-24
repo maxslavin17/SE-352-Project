4cdccfb7574fb1f7f4695092f9d1a73e7f66e919
joeyudongs <yudongs13@gmail.com>
2022-04-24 14:39:44 -0500

    update

--- springboot/src/main/java/com/example/springboot/controller/UserController.java
+++ springboot/src/main/java/com/example/springboot/controller/UserController.java
@@ -2,21 +2,35 @@ package com.example.springboot.controller;
 
 import com.example.springboot.entity.User;
 import com.example.springboot.mapper.UserMapper;
+import com.example.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserMapper userMapper;
 
-    @GetMapping("/")
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
     public List<User> index() {
         List<User> all = userMapper.findAll();
         return all;
+    }
+
+    @PostMapping
+    public Integer save(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Integer delete(@PathVariable Integer id) {
+        return userMapper.deleteById(id);
     }
 }
--- springboot/src/main/java/com/example/springboot/mapper/UserMapper.java
+++ springboot/src/main/java/com/example/springboot/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package com.example.springboot.mapper;
 
 import com.example.springboot.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -11,4 +10,13 @@ public interface UserMapper {
 
     @Select("SELECT * from sys_user")
     List<User> findAll();
+
+    @Insert("INSERT into sys_user(username, password,lastname,email,phone,address) VALUES (#{username}, #{password}," +
+            " #{lastname}, #{email}, #{phone}, #{address})")
+    int insert(User user);
+
+    int update(User user);
+
+    @Delete("delete from sys_user where id = #{id}")
+    Integer deleteById(@Param("id") Integer id);
 }
