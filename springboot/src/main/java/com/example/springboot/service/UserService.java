61147fe46f1fa9e017ffc30ab5c3d5899cb7335e
joeyudongs <yudongs13@gmail.com>
2022-04-24 19:04:20 -0500

    update

--- springboot/src/main/java/com/example/springboot/service/UserService.java
+++ springboot/src/main/java/com/example/springboot/service/UserService.java
@@ -20,25 +20,3 @@ public class UserService {
     }
 }
 
-//package com.example.springboot.service;
-//
-//        import com.example.springboot.entity.User;
-//        import com.example.springboot.mapper.UserMapper;
-//        import org.springframework.beans.factory.annotation.Autowired;
-//        import org.springframework.stereotype.Service;
-//
-//@Service
-//public class UserService {
-//
-//    @Autowired
-//    private UserMapper userMapper;
-//
-//    public int save(User user) {
-//        if (user.getId() == null) {
-//            return userMapper.insert(user);
-//        } else {
-//            return userMapper.update(user);
-//        }
-//    }
-//}
-
