e33b2947f210f0657c4b3ce798ae8c6cb918ea5f
joeyudongs <yudongs13@gmail.com>
2022-04-27 10:22:40 -0500

    refactor frontend

---
+++ frontend/src/components/Aside.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-menu :default-openeds="['1', '3']" style="min-height: 100%; overflow-x: hidden"
+           background-color="rgb(48, 65, 86)"
+           text-color="#fff"
+           active-text-color="#ffd04b"
+           :collapse-transition="false"
+           :collapse="isCollapse"
+           router
+  >
+    <div style="height: 60px; line-height: 60px; text-align: center">
+      <img src="../assets/logo1.jpeg" alt="" style="width: 20px; position: relative; top: 5px; right: 5px">
+      <b style="color: white" v-show="logoTextShow">CAMPUS CONNECT</b>
+    </div>
+    <el-menu-item index="/home">
+      <template slot="title">
+        <i class="el-icon-house"></i>
+        <span slot="title">HOME</span>
+      </template>
+    </el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-menu"></i>
+        <span slot="title">SYSTEM</span>
+      </template>
+      <el-menu-item index="/user">
+        <i class="el-icon-s-custom"></i>
+        <span slot="title">USER</span>
+      </el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Aside",
+  props: {
+    isCollapse: Boolean,
+    logoTextShow: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import store from "@/store";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: () => import('../views/Manage.vue'),
+    redirect: "/home",
+    children: [
+      { path: 'home', name: 'Home', component: () => import('../views/Home.vue')},
+      { path: 'user', name: 'User', component: () => import('../views/User.vue')},
+    ]
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  localStorage.setItem("currentPathName", to.name)
+  store.commit("setPath")
+  next()
 })
 
 export default router
