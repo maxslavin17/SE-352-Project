5e7dd4f71d245e5b1a9987dbffba32ad79e1acee
joeyudongs <yudongs13@gmail.com>
2022-04-29 17:44:14 -0500

    clear redundant

--- springboot/src/main/java/com/example/springboot/entity/User.java
+++ springboot/src/main/java/com/example/springboot/entity/User.java
@@ -1,11 +1,6 @@
 package com.example.springboot.entity;
 
 import javax.persistence.*;
-import com.baomidou.mybatisplus.annotation.IdType;
-import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
@@ -26,28 +21,3 @@ public class User {
     private String address;
     //private LocalDateTime createTime;
 }
-
-//package com.example.springboot.entity;
-//
-//import com.baomidou.mybatisplus.annotation.IdType;
-//import java.time.LocalDateTime;
-//import com.baomidou.mybatisplus.annotation.TableField;
-//import com.baomidou.mybatisplus.annotation.TableId;
-//import com.baomidou.mybatisplus.annotation.TableName;
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//import lombok.Data;
-//
-//@Data
-//@TableName(value = "sys_user")
-//public class User {
-//    @TableId(type = IdType.AUTO)
-//    private Integer id;
-//    private String username;
-//    @JsonIgnore
-//    private String password;
-//    private String lastname;
-//    private String email;
-//    private String phone;
-//    private String address;
-//    //private LocalDateTime createTime;
-//}
--- springboot/src/main/java/com/example/springboot/service/UserService.java
+++ springboot/src/main/java/com/example/springboot/service/UserService.java
@@ -12,34 +12,3 @@ public interface UserService {
     public boolean deleteById(int id);
     public boolean deleteBatchById(List<Integer> ids);
 }
-//package com.example.springboot.service;
-//
-//import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-//import com.example.springboot.entity.User;
-//import com.example.springboot.mapper.UserMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-////@Service
-//public class UserService extends ServiceImpl<UserMapper, User> {
-//    public boolean saveUser(User user) {
-//        if (user.getId() == null) {
-//            return save(user);
-//        } else {
-//            return updateById(user);
-//        }
-//        //return saveOrUpdate(user);
-//    }
-//
-////    @Autowired
-////    private UserMapper userMapper;
-////
-////    public int save(User user) {
-////        if (user.getId() == null) {
-////            return userMapper.insert(user);
-////        } else {
-////            return userMapper.update(user);
-////        }
-////    }
-//}
-
